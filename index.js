5edcf9f73c6afbfff8294150a173e282a208eeae
SantaMarie <59677546+SantaMarie@users.noreply.github.com>
2023-03-20 08:33:23 -0700

    adding index.js file

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat (name) {
+    cats.push(name)
+    return cats
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+    return cats
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+    return cats
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
+}
+
+function appendCat(name){
+    const cats = ['Milo', 'Otis', 'Garfield']
+    const newCats = [...cats,name];
+    return newCats
+}
+
+function prependCat(name){
+    const cats = ['Milo', 'Otis', 'Garfield']
+    const newCats = [name,...cats]
+    return newCats;
+}
+
+function  removeLastCat(){
+    const cats = ['Milo', 'Otis', 'Garfield']
+    const newCats = cats.slice(0,2)
+    return newCats;
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
